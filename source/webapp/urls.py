d92e093958ae7495e3905692b788d747f213a1c2
Napopegu <142897234+Napopegu@users.noreply.github.com>
2024-01-12 02:10:09 +0600

    Реализовал удаление задачи

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import IndexView, IssueView, IssueCreatView, IssueUpdateView
+from webapp.views import IndexView, IssueView, IssueCreatView, IssueUpdateView, IssueDeleteView
 
 urlpatterns = [
 path('', IndexView.as_view(), name='index'),
 path('issues/<int:pk>/', IssueView.as_view(), name='issue_view'),
 path('issues/add/', IssueCreatView.as_view(), name='issue_add_view'),
-path('issues/<int:pk>/edit/', IssueUpdateView.as_view(), name='issue_update_view')
+path('issues/<int:pk>/edit/', IssueUpdateView.as_view(), name='issue_update_view'),
+path('issues/<int:pk>/delete/', IssueDeleteView.as_view(), name='issue_delete_view')
 ]
