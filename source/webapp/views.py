6ba5674b0cb60fae394ca46ba110d68dd7e3ca77
Napopegu <142897234+Napopegu@users.noreply.github.com>
2024-01-12 01:34:14 +0600

    Реализовал добавление задачи

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import  TemplateView
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import TemplateView
 from webapp.models import Issue
+from webapp.forms import IssueForm
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -10,11 +12,34 @@ class IndexView(TemplateView):
         issues = Issue.objects.all()
         context['issues'] = issues
         return context
+
+
 class IssueView(TemplateView):
     template_name = 'issue_view.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        issue = get_object_or_404(Issue,pk=self.kwargs.get('pk'))
+        issue = get_object_or_404(Issue, pk=self.kwargs.get('pk'))
         context['issue'] = issue
         return context
+
+
+class IssueCreatView(TemplateView):
+    template_name = 'issue_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = IssueForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = IssueForm(data=request.POST)
+        if form.is_valid():
+            issue = Issue.objects.create(
+                summary=form.cleaned_data['summary'],
+                description=form.cleaned_data['description'],
+                status=form.cleaned_data['status'],
+                type=form.cleaned_data['type']
+            )
+            return redirect('issue_view', pk=issue.pk)
+        return render(request, 'issue_create.html', {'form': form})
